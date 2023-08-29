a7d0317fb653d98cbecdf033a4e0a9604e233e50
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-29 15:56:27 +0000

    #removed fb.app.id

--- app/episode/[slug]/page.tsx
+++ app/episode/[slug]/page.tsx
@@ -63,7 +63,6 @@ export const revalidate = 60;
           ],
           locale: 'en_US',
           type: 'website',
-          "fb:app_id": process.env.FB_APP_ID,
 
         },
       // Add the 'og:image' tag to the metadata
