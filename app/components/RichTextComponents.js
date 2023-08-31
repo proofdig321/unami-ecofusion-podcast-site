d320159dd2ff9b123b9a41e5a4f48a14082b3a50
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-31 13:32:17 +0000

    #edits

--- app/components/RichTextComponents.js
+++ app/components/RichTextComponents.js
@@ -1,8 +1,5 @@
 'use client'
 
-import Image from 'next/image';
-import Link from 'next/link';
-import urlFor from '../../sanity/lib/urlFor'
 
 import React from 'react';
 import getYouTubeId from 'get-youtube-id';
