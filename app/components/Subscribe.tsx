dda1d308e9086dbc2416d40a02a46f0488532ff5
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-09-08 01:55:19 +0000

    #nomodifications

--- app/components/Subscribe.tsx
+++ app/components/Subscribe.tsx
@@ -1,37 +1,5 @@
 'use client'
 
-{/*
-'use client'
-
-import Image from "next/image"
-import Container from "./Container"
-import Heading from "./Heading"
-
-
-const Subscribe = () => {
-  return (
-    <div className="bg-primary-1 px-4 sm:px-16 py-16">
-        <Container>
-      <div className="max-w-5xl mx-auto">
-            <Heading center title="Subsribe for new episode in your inbox" color="text-black" />
-            <div className="flex flex-col md:flex-row gap-4 md:gap-0 justify-center items-center py-8">
-                <input type="email" className="bg-primary-1 text-black border placeholder:text-black p-4 w-full md:w-[400px] outline-none focus:outline focus:outline-2 focus:outline-black" placeholder="Enter your email address" />
-                <button className="bg-black text-primary-1 w-full md:w-fit block p-4 md:px-8">
-                    Subscribe
-                </button>
-            </div>
-      </div>
-        </Container>
-
-        <Image src='/flower.png' alt='flower' width={50} height={50} className="object-contain absolute" />
-
-    </div>
-  )
-}
-
-export default Subscribe
-*/}
-
 import React from "react";
 import { useFormFields, useMailChimpForm } from "use-mailchimp-form";
 
@@ -88,3 +56,37 @@ const Subscribe = () => {
 };
 
 export default Subscribe;
+
+
+{/*
+'use client'
+
+import Image from "next/image"
+import Container from "./Container"
+import Heading from "./Heading"
+
+
+const Subscribe = () => {
+  return (
+    <div className="bg-primary-1 px-4 sm:px-16 py-16">
+        <Container>
+      <div className="max-w-5xl mx-auto">
+            <Heading center title="Subsribe for new episode in your inbox" color="text-black" />
+            <div className="flex flex-col md:flex-row gap-4 md:gap-0 justify-center items-center py-8">
+                <input type="email" className="bg-primary-1 text-black border placeholder:text-black p-4 w-full md:w-[400px] outline-none focus:outline focus:outline-2 focus:outline-black" placeholder="Enter your email address" />
+                <button className="bg-black text-primary-1 w-full md:w-fit block p-4 md:px-8">
+                    Subscribe
+                </button>
+            </div>
+      </div>
+        </Container>
+
+        <Image src='/flower.png' alt='flower' width={50} height={50} className="object-contain absolute" />
+
+    </div>
+  )
+}
+
+export default Subscribe
+*/}
+
